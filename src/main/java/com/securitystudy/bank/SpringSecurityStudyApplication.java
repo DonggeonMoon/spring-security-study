791183a6b21837303e1bfade2828252b674f3809
Donggeon Moon <nullsafer@naver.com>
2023-09-21 17:00:13 +0900

    chore: 패키지 구조 변경

--- src/main/java/com/securitystudy/bank/SpringSecurityStudyApplication.java
+++ src/main/java/com/securitystudy/bank/SpringSecurityStudyApplication.java
@@ -1,11 +1,10 @@
-package com.securitystudy;
+package com.securitystudy.bank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("com.securitystudy.controller")
 public class SpringSecurityStudyApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringSecurityStudyApplication.class, args);
--- src/main/java/com/securitystudy/basic/controller/WelcomeController.java
+++ src/main/java/com/securitystudy/basic/controller/WelcomeController.java
@@ -1,12 +1,22 @@
-package com.securitystudy.controller;
+package com.securitystudy.basic.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class WelcomeController {
     @GetMapping("/welcome")
     public String sayWelcome() {
         return "Welcome to Spring Application with Security";
+    }
+
+    public static void main(String[] args) {
+        List<String> stringList = new ArrayList<>();
+        stringList.add("ddfa");
+
+        stringList.clear();
     }
 }
