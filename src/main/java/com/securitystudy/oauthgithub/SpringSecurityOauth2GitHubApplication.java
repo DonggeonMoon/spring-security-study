aa6cb3f6398072c052638736657670c5453a14a6
Donggeon Moon <nullsafer@naver.com>
2023-10-13 13:13:23 +0900

    feat: Github OAuth2 사용 코드 추가

---
+++ src/main/java/com/securitystudy/oauthgithub/SpringSecurityOauth2GitHubApplication.java
@@ -0,0 +1,11 @@
+package com.securitystudy.oauthgithub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityOauth2GitHubApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityOauth2GitHubApplication.class, args);
+	}
+}
---
+++ src/main/java/com/securitystudy/oauthgithub/config/SpringSecurityOauth2GitHubConfig.java
@@ -0,0 +1,19 @@
+package com.securitystudy.oauthgithub.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@RequiredArgsConstructor
+public class SpringSecurityOauth2GitHubConfig {
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated())
+                .oauth2Login(Customizer.withDefaults());
+        return http.build();
+    }
+}
